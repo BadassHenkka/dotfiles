968ea2983ac09b0a78dcd81731d361136954c6f9
Henri Kestiö <henri.kestio@gmail.com>
2021-05-25 19:03:21 +0300

    Clean up apps.sh code

--- os/apps.sh
+++ os/apps.sh
@@ -5,51 +5,115 @@ declare DOT=$HOME/dotfiles
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "$DOT/setup/utils.sh"
 
-print_in_purple "\n Install applications \n\n"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# TLP for laptop battery management
+install_tlp_battery_management() {
 
-sudo dnf install tlp tlp-rdw
+        print_in_purple "\n • Installing tlp battery management \n\n"
 
-thinkpad_pkgs() {
-
-	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-	sudo dnf install https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm
-	sudo dnf install kernel-devel akmod-acpi_call akmod-tp_smapi
+        sudo dnf install tlp tlp-rdw
 
 }
 
-print_in_purple "\n If you're setting up a Thinkpad - select 1 \n"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-select yn in "thinkpad" "other"; do
-        case $yn in
-                thinkpad ) thinkpad_pkgs; break;;
-                other ) break;;
-        esac
-done
+thinkpad_pkgs() {
 
-# Multimedia codecs
+        print_in_purple "\n • Installing thinkpad packages \n\n"
 
-print_in_purple "\n Some multimedia codecs... \n\n"
+        sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+        sudo dnf install https://repo.linrunner.de/fedora/tlp/repos/releases/tlp-release.fc$(rpm -E %fedora).noarch.rpm
+        sudo dnf install kernel-devel akmod-acpi_call akmod-tp_smapi
 
-sudo dnf groupupdate sound-and-video
-sudo dnf install -y libdvdcss
-sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-sudo dnf install -y lame\* --exclude=lame-devel
-sudo dnf group upgrade --with-optional Multimedia
+}
 
-# Gitkraken
-flatpak install -y gitkraken
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# VSCode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-sudo dnf install -y code
+optional_thinkpad_pkg_install() {
 
-echo 'fs.inotify.max_user_watches=524288' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
+        print_in_purple "\n • If you're setting up a Thinkpad - select 1 \n"
 
-# VLC
+        select yn in "thinkpad" "other"; do
+                case $yn in
+                        thinkpad ) thinkpad_pkgs; break;;
+                        other ) break;;
+                esac
+        done
 
-sudo dnf install -y vlc
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_multimedia_codecs() {
+
+        print_in_purple "\n • Installing multimedia codecs... \n\n"
+
+        sudo dnf groupupdate sound-and-video
+        sudo dnf install -y libdvdcss
+        sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+        sudo dnf install -y lame\* --exclude=lame-devel
+        sudo dnf group upgrade --with-optional Multimedia
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_git_kraken_flatpak() {
+
+        print_in_purple "\n • Installing Git Kraken \n\n"
+
+        flatpak install -y gitkraken
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_VSCode_and_set_inotify_max_user_watches() {
+
+        print_in_purple "\n • Installing VSCode \n\n"
+
+        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+        sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+        sudo dnf check-update
+        sudo dnf install -y code
+
+        echo 'fs.inotify.max_user_watches=524288' | sudo tee -a /etc/sysctl.conf
+        sudo sysctl -p
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_VLC() {
+
+        print_in_purple "\n • Installing VLC \n\n"
+
+        sudo dnf install -y vlc
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+        print_in_purple "\n • Installing applications \n\n"
+
+        install_tlp_battery_management
+
+        optional_thinkpad_pkg_install
+
+        install_multimedia_codecs
+
+        install_git_kraken_flatpak
+
+        install_VSCode_and_set_inotify_max_user_watches
+
+        install_VLC
+
+}
+
+main
